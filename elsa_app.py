86eac845a2232c1ac69169b26d53455e43f60b64
Suurpere <mliblik@gmail.com>
2025-11-29 16:09:11 +0200

    Failist lugemine on eraldatud põhikoodist. Põhikoodis failist lugemise kasutamiseks on see imporditud.

--- elsa_app.py
+++ elsa_app.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from konstandid import *
 from abifunktsioonid import *
 from selgitamine import *
+from lugemine import *
 
 # --- Üldine seadistus ---
 st.set_page_config(page_title="Kulutuste analüüs ELSA", layout="wide")
@@ -17,26 +18,12 @@ mode = st.sidebar.radio(
 )
 
 # --- Selgitus / probleemikirjeldus ---
-
 if mode == "Selgitus":
     selgita()
 
 # --- CSV lugemine (Failist lugeda) ---
-
 elif mode == "Failist lugemine":
-    st.header("📂 Loe CSV-faili sisu")
-
-    uploaded_file = st.file_uploader("Vali CSV-fail", type=["csv"], key="read_file")
-
-    if uploaded_file is not None:
-        try:
-            df = pd.read_csv(uploaded_file, encoding="utf-8")
-            st.success(f"Fail loetud: {uploaded_file.name}")
-            st.dataframe(df)
-        except Exception as e:
-            st.error(f"Faili lugemisel tekkis viga: {e}")
-    else:
-        st.info("Palun vali CSV-fail, mida soovid vaadata.")
+    loe()
 
 # --- CSV loomine / kirjutamine (Faili kirjutada) ---
 
---
+++ lugemine.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+def loe():
+    st.header("📂 Loe CSV-faili sisu")
+
+    uploaded_file = st.file_uploader("Vali CSV-fail", type=["csv"], key="read_file")
+
+    if uploaded_file is not None:
+        try:
+            df = pd.read_csv(uploaded_file, encoding="utf-8")
+            st.success(f"Fail loetud: {uploaded_file.name}")
+            st.dataframe(df)
+        except Exception as e:
+            st.error(f"Faili lugemisel tekkis viga: {e}")
+    else:
+        st.info("Palun vali CSV-fail, mida soovid vaadata.")
